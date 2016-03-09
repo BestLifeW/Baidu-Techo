102f90c2884de71f25bb8220cd1d3919ff1bb295
LCINA <870206584@qq.com>
2016-03-09 18:03:38 +0800

    Revert "开发团队首页"
    This reverts commit bd6620e70e2fe2770ce796bfdd7ded7c655ca4a0.

--- index.js
+++
@@ -1,8 +0,0 @@
-$(window).load(function(){
-	var availHeight = $(window).height();
-	$("body").css("height",availHeight);
-});
-window.onresize=function(){
-	var availHeight = $(window).height();
-	$("body").css("height",availHeight);
-}  
